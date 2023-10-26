463fb8ff51e375adb6467d215f2f5898d78eec05
sowmik-sec <sowmik.sec@gmail.com>
2023-10-26 06:31:19 +0600

    create postconstruct method

--- src/main/java/com/sowmik/restcrud/rest/StudentRestController.java
+++ src/main/java/com/sowmik/restcrud/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.sowmik.restcrud.rest;
 
 import com.sowmik.restcrud.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +12,21 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    // define endpoint for "/students" - return a list of students
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> theStudents = new ArrayList<>();
+    private List<Student> theStudents;
+    // define @PostConstruct to load the student data ... only once
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
 
         theStudents.add(new Student("John", "Doe"));
         theStudents.add(new Student("Barak", "Obama"));
         theStudents.add(new Student("Vladimir", "Putin"));
+    }
+
+    // define endpoint for "/students" - return a list of students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+
 
         return theStudents;
     }
